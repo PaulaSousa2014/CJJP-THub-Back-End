bde6ab8648e269ceaf265ace1a5dee6adc22f3e8
JPrimee <javierpsantos99@gmail.com>
2023-05-04 13:05:40 +0200

    User CRUD

---
+++ FINAL-PROJECT-THUB/src/main/java/THUBPROJECT/service/UserService.java
@@ -0,0 +1,39 @@
+package THUBPROJECT.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.*;
+
+import THUBPROJECT.dao.UserDAO;
+import THUBPROJECT.dto.User;
+
+@Service // Assign Service
+public class UserService {
+
+	// Load DAO
+	@Autowired
+	UserDAO userDAO;
+
+	// CRUD functions
+	public List<User> listUsers() {
+		return userDAO.findAll();
+	}
+
+	public User saveUser(User user) {
+		return userDAO.save(user);
+	}
+
+	public User userById(Long id) {
+		return userDAO.findById(id).get();
+	}
+
+	public User updateUser(User user) {
+		return userDAO.save(user);
+	}
+
+	public void deleteUser(Long id) {
+		userDAO.deleteById(id);
+	}
+
+}
