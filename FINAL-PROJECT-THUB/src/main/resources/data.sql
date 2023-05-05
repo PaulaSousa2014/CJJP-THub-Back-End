046dbbdb00e97046f1f3494457f1388bf63d75eb
Paula Sousa <paulasousa137@gmail.com>
2023-05-05 12:51:17 +0200

    INSERT social

--- FINAL-PROJECT-THUB/src/main/resources/data.sql
+++ FINAL-PROJECT-THUB/src/main/resources/data.sql
@@ -204,7 +204,12 @@ INSERT INTO private_messages (content, sender, receiver) VALUES ('Message 4', 4,
 INSERT INTO posts (title, content, creator) VALUES ('I love Cats!', 'Cats are the best thing ever, I swear to god I have over 400 cats adopted already', 2);
 INSERT INTO posts (title, content, creator) VALUES ('Test', 'Testing', 3);
 
+INSERT INTO likes (user_liked, post_liked) VALUES (4, 1);
+
 INSERT INTO comments (content, comment_by, in_post) VALUES ('Theyre so cute! :D', 4, 1);
 INSERT INTO comments (content, comment_by, in_post) VALUES ('Meh', 1, 1);
 
-INSERT INTO likes (user_liked, post_liked) VALUES (4, 1);
+INSERT INTO socials (title, description, theme) VALUES ('Developers Memes', 'description 1', 'Memes');
+INSERT INTO socials (title, description, theme) VALUES ('Title2', 'description 2', 'Memes');
+INSERT INTO socials (title, description, theme) VALUES ('FAQ', 'description 3', 'FAQ-THub');
+INSERT INTO socials (title, description, theme) VALUES ('Title4', 'description 4', 'Theme4');
